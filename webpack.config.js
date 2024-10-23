e4d29adeb8eb69ba47455e6dcd2a3ca92f8ce7e3
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-10-23 16:10:22 +0300

    docker updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,52 @@
-new ModuleFederationPlugin({
-    name: 'microB',
-    filename: 'remoteEntry.js',
-    exposes: {
-      './App': './src/App',
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-  });
-  
+    port: 3000,
+    hot: false,
+    liveReload: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'microB',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        'react-dom': {
+          singleton: true,
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: 'index.html',
+    }),
+  ],
+};
